994ba46fc87a77f9afdb49b02de725b09e7756aa
trecem <36710023+trecem@users.noreply.github.com>
2024-09-05 21:53:34 -0600

    buil gradle

--- src/main/java/com/construclick/construclick/model/Contacto.java
+++ src/main/java/com/construclick/construclick/model/Contacto.java
@@ -2,12 +2,7 @@ package com.construclick.construclick.model;
 
 import java.util.Objects;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "contacto")
